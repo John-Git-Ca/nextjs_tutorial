c154898daf0a0cd81df696231fdd5ab7402355c4
John <john.lizuhua@outlook.com>
2022-02-20 19:14:29 -0500

    correct error

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,5 +1,4 @@
 import MeetupDetail from '../components/meetups/MeetupDetail'
-import { MongoClient } from 'mongodb'
 const MeetupDetails = () => {
 
 
@@ -37,14 +36,6 @@ export const getStaticPaths = async() => {
   }
 }
 
-export const getStaticPaths = async () => {
-  
-  const client = await MongoClient.connect('mongodb://localhost:27017/nexttutorial')
-  const db = client.db()
-  
-  const meetupCollection = db.collection('nexttutorial')
-  const meetups = await meetupCollection.find({}, {_id: 1}).toArray()
-}
 
 export const getStaticProps = async (context) => {
 
